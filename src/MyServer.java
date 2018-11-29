fed8614a780bf9da917309103db28f50dfc644b6
AvivHK <avivhkn@gmail.com>
2018-11-29 16:34:48 +0200

    Patam

--- src/MyServer.java
+++ src/MyServer.java
@@ -1,25 +1,28 @@
-
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-
-
-
-
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 public class MyServer implements Server {
     private ServerSocket serverSocket;
     private int port;
-    private volatile boolean stop = false;
+    private boolean stop = false;
+    ThreadPoolExecutor threadPoolExecutor;
 
-    public MyServer(){}
-
-    public MyServer(int port) {
+    public MyServer(int port, int num) {
         this.port = port;
+        this.threadPoolExecutor = new ThreadPoolExecutor(1, num, 10, TimeUnit.SECONDS,
+                new PriorityBlockingQueue<Runnable>());
     }
 
     @Override
-    public void start(ClientHandler clientHandler) {
+    public void stop() {
+        stop = true;
+    }
+
+    public void start(ClientHandler clientHandler) throws IOException {
         new Thread(() -> {
             try {
                 runServer(clientHandler);
@@ -29,35 +32,55 @@ public class MyServer implements Server {
     }
 
 
-    @Override
-    public void stop() {
-        this.stop = true;
-    }
-
-    @Override
     public void runServer(ClientHandler clientHandler) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(port);
-        serverSocket.setSoTimeout(5000);
+
+        serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(10000);
+        System.out.println("Start Server.");
 
         while (!stop) {
             try {
                 Socket aClient = serverSocket.accept();
+                System.out.println("Connect to client");
+                int clientPriority = 0;
+                do {
+                    clientPriority = aClient.getInputStream().available();
+                } while (clientPriority == 0);
+                threadPoolExecutor.execute(new PriorityQRunnable(clientPriority) {
 
-                clientHandler.handleClient(aClient.getInputStream(), aClient.getOutputStream());
+                    @Override
+                    public void run() {
+                        try {
+                            clientHandler.handleClient(aClient.getInputStream(), aClient.getOutputStream());
+                            aClient.close();
+                            System.out.println("Disconnected.");
+                        } catch (SocketTimeoutException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }catch (IOException e) {
 
-                aClient.close();
-
-            } catch (SocketTimeoutException e) {
-                //System.out.println(e.getMessage());
-            } catch (IOException e) {
-                //e.printStackTrace();
+            }
+            System.out.println(threadPoolExecutor.getActiveCount());
+            if(threadPoolExecutor.getActiveCount() == 0) {
+                this.stop();
             }
         }
+
+        threadPoolExecutor.shutdown();
         serverSocket.close();
+        System.out.println("All clients had been handled");
+
+        try {
+            threadPoolExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 
 
-
 }
-
